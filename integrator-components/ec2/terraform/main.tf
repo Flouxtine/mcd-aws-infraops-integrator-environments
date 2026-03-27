8bf66286a82317d43b9e4a19bf982eba9b786705
Zane <zane@ZanedeMacBook-Pro.local>
2026-03-27 16:31:12 +0800

    feat: add production

--- integrator-components/ec2/terraform/main.tf
+++ integrator-components/ec2/terraform/main.tf
@@ -25,7 +25,7 @@ data "aws_security_groups" "by_names" {
 module "ec2" {
 
   for_each = var.create_ec2_config
-  source   = "git::ssh://github-flouxtine:Flouxtine/aws-terraform-base-module.git//terraform-aws-ec2?ref=main"
+  source   = "git::https://github.com/Flouxtine/aws-terraform-base-module.git//terraform-aws-ec2?ref=main"
 
   # General Configuration
   create = try(each.value.create, true)
